d32f8cb3a3378a1293a06c36463ccdd43a8320c2
Peter Bowers <pgbowers@gmail.com>
2022-12-05 08:06:06 -0400

    basic working

--- script.js
+++ script.js
@@ -4,13 +4,20 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 
+// This sets the canvas size to the entire window size...bad!
+//canvas.width = window.innerWidth;
+//canvas.height = window.innerHeight;
+
 let x = canvas.width/2;
-let y = canvas.height-30;
-let dx = 3;
-let dy = 3.5;
-const ballRadius = 10;
-const paddleHeight = 10;
-const paddleWidth = 75;
+let y = canvas.height/2;
+//let dx = Math.floor(Math.random() * (3 - 7
+let dx = Math.floor(Math.random() * (10 - (-10))) + (-10);
+//let dy = -3.5;
+let dy = Math.floor(Math.random() * (10 - (-10))) + (-10);
+//let dy = Math.floor(Math.random() * (3 - 7));
+const ballRadius = 40;
+const paddleHeight = 28;
+const paddleWidth = 104;
 let paddleX = (canvas.width - paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
@@ -18,20 +25,26 @@ let paddleSpeed = 7;
 let score = 0;
 let lives = 3;
 
+//console.log("Canvas width: " + canvas.width);
+//console.log("Canvas height: " + canvas.height);
+//console.log("Window inner width: " + window.innerWidth);
+//console.log("Window inner height: " + window.innerHeight);
+
 // Defining the bricks
-const brickRowCount = 3;
-const brickColumnCount = 5;
+const brickRowCount = 5;
+const brickColumnCount = 9;
 const brickWidth = 75;
 const brickHeight = 20;
 const brickPadding = 10;
-const brickOffsetTop = 30;
-const brickOffsetLeft = 30;
+const brickOffsetTop = 40;
+const brickOffsetLeft = 35;
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
-document.addEventListener("mousemove", mouseMoveHandler, false);
+canvas.addEventListener("mousemove", mouseMoveHandler, false);
 
 function mouseMoveHandler(event) {
+    // offsetLeft: number of pixels that canvas is offset to the left from the parent.
     const relativeX = event.clientX - canvas.offsetLeft;
     if (relativeX > 0 && relativeX < canvas.width) {
         paddleX = relativeX - paddleWidth/2;
@@ -47,9 +60,9 @@ for ( let c = 0; c < brickColumnCount; c++) {
 }
 
 function drawLives(){
-    ctx.font = "16px Arial";
-    ctx.fillStyle = "#0095DD";
-    ctx.fillText('Lives: ' + lives, 420, 20);
+    ctx.font = "24px Arial";
+    ctx.fillStyle = "red";
+    ctx.fillText('Lives: ' + lives, canvas.width - (canvas.width -700), canvas.height - 570);
 }
 
 function drawBricks() {
@@ -65,11 +78,14 @@ function drawBricks() {
             bricks[c][r].y = brickY;
 
             // Draw a brick
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "#0095dd";
-            ctx.fill();
-            ctx.closePath();
+            const brick = new Image();
+            brick.src = "assets/PURPLEBRICK.png";
+            ctx.drawImage(brick, brickX, brickY);
+            //ctx.beginPath();
+            //ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            //ctx.fillStyle = "#0095dd";
+            //ctx.fill();
+            //ctx.closePath();
             }
         }
     }
@@ -92,25 +108,32 @@ function keyUpHandler(event) {
 }
 
 function drawBall(){
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = 'blue';
-    ctx.fill();
-    ctx.closePath(); 
+    const ball = new Image();
+    ball.src = "assets/PURPLEBALL.png";
+    ctx.drawImage(ball, x, y);
+    //ctx.beginPath();
+    //ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    //ctx.fillStyle = 'blue';
+    //ctx.fill();
+    //ctx.closePath(); 
 }
 
 function drawPaddle(){
-    ctx.beginPath();
-    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = 'green';
-    ctx.fill();
-    ctx.closePath();
+    //ctx.beginPath();
+    //ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    //ctx.fillStyle = 'green';
+    //ctx.fill();
+    //ctx.closePath();
+    const paddle = new Image();
+    paddle.src = "assets/REDPADDLEM.png";
+    ctx.drawImage(paddle, paddleX, canvas.height - paddleHeight);
+    //document.div.appendChild(img);
 }
 
 function drawScore() {
-    ctx.font = "16px Arial";
-    ctx.fillStyle = "#0095DD";
-    ctx.fillText('Score: ' + score, 8, 20);
+    ctx.font = "24px Arial";
+    ctx.fillStyle = "red";
+    ctx.fillText('Score: ' + score, canvas.width - 780, canvas.height - 570);
 }
 
 function collisionDetection(){
@@ -144,20 +167,23 @@ function draw() {
     drawPaddle(); 
     drawScore();
     collisionDetection(); 
-    drawLives();
-   // drawScore();  
+    drawLives();   
 
-    // Bounce off three walls
+    // Bounce off both sides
     if( x + dx < ballRadius || x + dx > canvas.width-ballRadius){
         dx = -dx; 
     }
+    // Bounce off the top
     if (y + dy < ballRadius){
         dy = -dy;     
     }
-    else if (y + dy > canvas.height - ballRadius){
+    // If the ball gets near the bottom of the canvas
+    else if (y + dy > canvas.height - paddleHeight){
+        // If the ball is contacting the paddle, bounce.
         if( x > paddleX && x < paddleX + paddleWidth){
             dy = -dy;
         }
+        // If the ball missed the paddle
         else {            
                 //ctx.fillStyle = 'red';
                 //ctx.font = "30px serif";
@@ -174,7 +200,7 @@ function draw() {
                     x = canvas.width / 2;
                     y = canvas.height - 30;
                     dx = 3;
-                    dy = 3.5;
+                    dy = -3.5;
                     paddleX = (canvas.width - paddleWidth) / 2;
                 }
                 	            
