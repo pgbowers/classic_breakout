1cffbc895ea3682c2c5143365ada82e871dbbca5
Peter Bowers <pgbowers@gmail.com>
2022-12-07 08:04:13 -0400

    Paddle bounce working

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 // Nov 25,2022: from the MDN game tutorial at 
 // https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript
 
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
+var canvas = document.getElementById('canvas1');
+var ctx = canvas.getContext('2d');
 
 // This sets the canvas size to the entire window size...bad!
 //canvas.width = window.innerWidth;
 //canvas.height = window.innerHeight;
 
-let x = canvas.width/2;
-let y = canvas.height/2;
-//let dx = Math.floor(Math.random() * (3 - 7
-let dx = Math.floor(Math.random() * (10 - (-10))) + (-10);
-//let dy = -3.5;
-let dy = Math.floor(Math.random() * (10 - (-10))) + (-10);
-//let dy = Math.floor(Math.random() * (3 - 7));
-const ballRadius = 40;
-const paddleHeight = 28;
-const paddleWidth = 104;
-let paddleX = (canvas.width - paddleWidth) / 2;
-let rightPressed = false;
-let leftPressed = false;
-let paddleSpeed = 7;
-let score = 0;
-let lives = 3;
+var x = canvas.width/2;
+var y = canvas.height/2;
+//var dx = Math.floor(Math.random() * (3 - 7
+//var dx = Math.floor(Math.random() * (10 - (-10))) + (-10);
+var dx = 3;
+var dy = -3.5;
+//var dy = Math.floor(Math.random() * (10 - (-10))) + (-10);
+//var dy = Math.floor(Math.random() * (3 - 7));
+var ballRadius = 12;
+var paddleHeight = 24;
+var paddleWidth = 104;
+var paddleX = (canvas.width - paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
+var paddleSpeed = 7;
+var score = 0;
+var lives = 3;
 
 //console.log("Canvas width: " + canvas.width);
 //console.log("Canvas height: " + canvas.height);
@@ -31,13 +32,13 @@ let lives = 3;
 //console.log("Window inner height: " + window.innerHeight);
 
 // Defining the bricks
-const brickRowCount = 5;
-const brickColumnCount = 9;
-const brickWidth = 75;
-const brickHeight = 20;
-const brickPadding = 10;
-const brickOffsetTop = 40;
-const brickOffsetLeft = 35;
+var brickRowCount = 9;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 40;
+var brickOffsetLeft = 35;
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -45,16 +46,18 @@ canvas.addEventListener("mousemove", mouseMoveHandler, false);
 
 function mouseMoveHandler(event) {
     // offsetLeft: number of pixels that canvas is offset to the left from the parent.
-    const relativeX = event.clientX - canvas.offsetLeft;
+    var relativeX = event.clientX - canvas.offsetLeft;
     if (relativeX > 0 && relativeX < canvas.width) {
         paddleX = relativeX - paddleWidth/2;
     }
 }
 
-const bricks = [];
-for ( let c = 0; c < brickColumnCount; c++) {
+console.log(canvas.offsetLeft);
+
+var bricks = [];
+for ( var c = 0; c < brickColumnCount; c++) {
     bricks[c] = [];
-    for (let r = 0; r < brickRowCount; r++) {
+    for (var r = 0; r < brickRowCount; r++) {
         bricks[c][r] = { x:0, y: 0, status: 1 };
     }
 }
@@ -66,19 +69,20 @@ function drawLives(){
 }
 
 function drawBricks() {
-    for (let c = 0; c < brickColumnCount; c++) {
-        for (let r = 0; r < brickRowCount; r++) {
+    for (var c = 0; c < brickColumnCount; c++) {
+        for (var r = 0; r < brickRowCount; r++) {
             // Don't draw a brick if the ball is in collision
-            if (bricks[c][r].status === 1) {
+            if (bricks[c][r].status == 1) {
             // Calculate position to draw each new block
-            const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
-            const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+            var brickX = (r * (brickWidth + brickPadding)) + brickOffsetLeft;
+            //var brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+            var brickY = (c * (brickHeight + brickPadding)) + brickOffsetTop;
 
             bricks[c][r].x = brickX;
             bricks[c][r].y = brickY;
 
             // Draw a brick
-            const brick = new Image();
+            var brick = new Image();
             brick.src = "assets/PURPLEBRICK.png";
             ctx.drawImage(brick, brickX, brickY);
             //ctx.beginPath();
@@ -108,14 +112,14 @@ function keyUpHandler(event) {
 }
 
 function drawBall(){
-    const ball = new Image();
-    ball.src = "assets/PURPLEBALL.png";
-    ctx.drawImage(ball, x, y);
-    //ctx.beginPath();
-    //ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    //ctx.fillStyle = 'blue';
-    //ctx.fill();
-    //ctx.closePath(); 
+    //var ball = new Image();
+    //ball.src = "assets/PURPLEBALL.png";
+    //ctx.drawImage(ball, x, y);
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+    ctx.closePath(); 
 }
 
 function drawPaddle(){
@@ -124,7 +128,7 @@ function drawPaddle(){
     //ctx.fillStyle = 'green';
     //ctx.fill();
     //ctx.closePath();
-    const paddle = new Image();
+    var paddle = new Image();
     paddle.src = "assets/REDPADDLEM.png";
     ctx.drawImage(paddle, paddleX, canvas.height - paddleHeight);
     //document.div.appendChild(img);
@@ -137,11 +141,11 @@ function drawScore() {
 }
 
 function collisionDetection(){
-    for (let c = 0; c < brickColumnCount; c++) {
-        for (let r = 0; r < brickRowCount; r++) {
-            const b = bricks[c][r];
+    for (var c = 0; c < brickColumnCount; c++) {
+        for (var r = 0; r < brickRowCount; r++) {
+            var b = bricks[c][r];
             // Calculations
-            if (b.status === 1) {
+            if (b.status == 1) {
                 if (x > b.x && 
                     x < b.x + brickWidth && 
                     y > b.y && 
@@ -158,7 +162,8 @@ function collisionDetection(){
         }
     }
 }
-
+console.log(canvas.height);
+console.log(paddleHeight);
 function draw() {
     // Drawing code
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -170,19 +175,21 @@ function draw() {
     drawLives();   
 
     // Bounce off both sides
-    if( x + dx < ballRadius || x + dx > canvas.width-ballRadius){
+    if( x + dx > canvas.width - ballRadius || x + dx < ballRadius){    
         dx = -dx; 
     }
     // Bounce off the top
-    if (y + dy < ballRadius){
+    if (y + dy < 0){
         dy = -dy;     
-    }
+    }    
     // If the ball gets near the bottom of the canvas
-    else if (y + dy > canvas.height - paddleHeight){
-        // If the ball is contacting the paddle, bounce.
+    else if (y + dy > canvas.height - paddleHeight){ 
+        // and the ball is over the paddle   
         if( x > paddleX && x < paddleX + paddleWidth){
-            dy = -dy;
-        }
+            // bounce off the top of the paddle
+            if(y + dy > (canvas.height - paddleHeight * 2)){
+            dy = -dy;           
+        }    }
         // If the ball missed the paddle
         else {            
                 //ctx.fillStyle = 'red';
@@ -198,7 +205,7 @@ function draw() {
                 document.location.reload();                
                 } else{
                     x = canvas.width / 2;
-                    y = canvas.height - 30;
+                    y = canvas.height / 2;
                     dx = 3;
                     dy = -3.5;
                     paddleX = (canvas.width - paddleWidth) / 2;
